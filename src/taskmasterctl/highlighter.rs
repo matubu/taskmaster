9633a4e3643136971b196c5f1344542dc3b175f2
u <hi@mathias.ninja>
2023-01-30 20:25:46 +0100

    highlighter

---
+++ src/taskmasterctl/highlighter.rs
@@ -0,0 +1,109 @@
+extern crate syntect;
+
+use syntect::easy::HighlightLines;
+use syntect::parsing::{SyntaxSet, SyntaxDefinition, Scope, ScopeStack, SyntaxSetBuilder};
+use syntect::highlighting::{Style, Theme, StyleModifier, Color, ThemeItem, ScopeSelector, ScopeSelectors, ThemeSettings};
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
+
+fn rgb(color: u64) -> Option<Color> {
+	Some(Color{
+		r: ((color >> 16) & 0xff) as u8,
+		g: ((color >> 8) & 0xff) as u8,
+		b: ((color >> 0) & 0xff) as u8,
+		a: 255
+	})
+}
+
+fn theme_item(scope: &str, color: u64) -> ThemeItem {
+	ThemeItem {
+		scope: ScopeSelectors { 
+			selectors: vec![ScopeSelector {
+				path: ScopeStack::from_vec(vec![Scope::new(scope).unwrap()]),
+				..Default::default()
+			}]
+		},
+		style: StyleModifier {
+			foreground: rgb(color),
+			..Default::default()
+		}
+	}
+}
+
+fn create_syntect_theme() -> Theme {
+	Theme {
+		scopes: vec![
+			theme_item("function", 0x73d0ff),
+			theme_item("keyword", 0xffad66),
+			theme_item("string", 0xd5ff80),
+			theme_item("string.escape", 0xdd5555),
+			theme_item("flag", 0xffd173),
+			theme_item("number", 0xdfbfff),
+		],
+		settings: ThemeSettings {
+			foreground: rgb(0xffffff),
+			..Default::default()
+		},
+		..Default::default()
+	}
+}
+
+fn create_syntect_syntax() -> SyntaxDefinition {
+	SyntaxDefinition::load_from_str(
+r#"name: taskmasterctl
+file_extensions: []
+scope: taskmasterctl
+
+contexts:
+  main:
+  - match: \b(help|status|start|stop|restart|list|load|unload|reload|logs)\b
+    scope: function
+  - match: \bglobal\b
+    scope: keyword
+  - match: '"'
+    push: string
+  - match: \s[+-]?([0-9_]+)\b
+    scope: number
+  - match: \s(--[a-zA-Z-]*)\b
+    scope: flag
+
+  string:
+  - meta_scope: string
+  - match: \\.
+    scope: string.escape
+  - match: '"'
+    pop: true"#, false, None).unwrap()
+}
+
+pub struct TaskmasterHighlighter {
+	theme: Theme,
+	syntax_set: SyntaxSet,
+}
+
+impl TaskmasterHighlighter {
+	pub fn new() -> TaskmasterHighlighter {
+		let theme = create_syntect_theme();
+		let syntax = create_syntect_syntax();
+
+		let mut syntax_set_builder = SyntaxSetBuilder::new();
+		syntax_set_builder.add(syntax);
+		let syntax_set = syntax_set_builder.build();
+
+		TaskmasterHighlighter {
+			theme,
+			syntax_set
+		}
+	}
+
+	pub fn highlight(&self, text: &str) -> String {
+		let mut h = HighlightLines::new(&self.syntax_set.syntaxes()[0], &self.theme);
+		let mut result = String::new();
+		
+		for line in LinesWithEndings::from(text) {
+			let ranges: Vec<(Style, &str)> = h.highlight_line(line, &self.syntax_set).unwrap();
+			let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
+			result += &escaped;
+		}
+
+		result + "\x1b[0m"
+	}
+}
--- src/taskmasterctl/main.rs
+++ src/taskmasterctl/main.rs
@@ -1,38 +1,64 @@
-extern crate rustyline;
+mod highlighter;
+use highlighter::{TaskmasterHighlighter};
 
 use rustyline::{
 	highlight::Highlighter,
-	completion::Completer,
-	hint::Hinter,
+	completion::{Completer, Candidate},
+	hint::{Hinter, Hint},
 	validate::{self, Validator},
-	line_buffer::LineBuffer, Helper
+	line_buffer::LineBuffer, Helper, config::Configurer
 };
 
 use std::borrow::Cow::{self, Owned};
 
-/*
-Commands:
-- help
+#[derive(Debug, PartialEq, Eq)]
+struct CommandHint {
+	display: String,
+	replace: String,
+}
 
-- global? status
-- global? start
-- global? stop
-- global? restart
+impl CommandHint {
+	fn new(text: &str) -> CommandHint {
+		CommandHint {
+			display: text.into(),
+			replace: text.into(),
+		}
+	}
 
-- list [all|program|processes|configs]
-- load
-- unload
-- reload
+	fn suffix(&self, strip_chars: usize) -> CommandHint {
+		CommandHint {
+			display: self.display.clone(),
+			replace: self.display[strip_chars..].to_owned(),
+		}
+	}
+}
 
-- log
-*/
+impl Hint for CommandHint {
+	fn display(&self) -> &str {
+		&self.replace
+	}
 
-// Completer + Hinter + Highlighter + Validator,
+	fn completion(&self) -> Option<&str> {
+		Some(&self.replace)
+	}
+}
+impl Candidate for CommandHint {
+	fn display(&self) -> &str {
+		&self.display
+	}
 
-struct TaskMasterHelper {}
+	fn replacement(&self) -> &str {
+		&self.replace
+	}
+}
 
-impl Completer for TaskMasterHelper {
-	type Candidate = String;
+struct TaskmasterHelper {
+	hints: Vec<CommandHint>,
+	highlighter: TaskmasterHighlighter,
+}
+
+impl Completer for TaskmasterHelper {
+	type Candidate = CommandHint;
 
 	fn complete(
 			&self, // FIXME should be `&mut self`
@@ -40,10 +66,18 @@ impl Completer for TaskMasterHelper {
 			pos: usize,
 			ctx: &rustyline::Context<'_>,
 		) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
-		let candidates = vec![
-			"help".to_owned(),
-			"global? status".to_owned(),
-		];
+		let candidates: Vec<CommandHint> = self.hints
+			.iter()
+			.filter_map(|hint| {
+				// expect hint after word complete, like redis cli, add condition:
+				// line.ends_with(" ")
+				if hint.display.starts_with(line) {
+					Some(hint.suffix(pos))
+				} else {
+					None
+				}
+			})
+			.collect();
 		Ok((pos, candidates))
 	}
 	fn update(&self, line: &mut LineBuffer, start: usize, elected: &str) {
@@ -52,17 +86,32 @@ impl Completer for TaskMasterHelper {
 	}
 }
 
-impl Hinter for TaskMasterHelper {
-	type Hint = String;
+impl Hinter for TaskmasterHelper {
+	type Hint = CommandHint;
 
-	fn hint(&self, line: &str, pos: usize, _ctx: &rustyline::Context<'_>) -> Option<String> {
-		Some("hint".to_owned())
+	fn hint(&self, line: &str, pos: usize, _ctx: &rustyline::Context<'_>) -> Option<CommandHint> {
+		if line.is_empty() || pos < line.len() {
+			return None;
+		}
+
+		self.hints
+			.iter()
+			.filter_map(|hint| {
+				// expect hint after word complete, like redis cli, add condition:
+				// line.ends_with(" ")
+				if hint.display.starts_with(line) {
+					Some(hint.suffix(pos))
+				} else {
+					None
+				}
+			})
+			.next()
 	}
 }
 
-impl Highlighter for TaskMasterHelper {
+impl Highlighter for TaskmasterHelper {
 	fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
-		Owned("\x1b[95m".to_owned() + line + "\x1b[0m")
+		Owned(self.highlighter.highlight(line))
 	}
 
 	fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
@@ -82,7 +131,7 @@ impl Highlighter for TaskMasterHelper {
 	}
 }
 
-impl Validator for TaskMasterHelper {
+impl Validator for TaskmasterHelper {
 	fn validate(&self, ctx: &mut validate::ValidationContext) -> rustyline::Result<validate::ValidationResult> {
 		use validate::ValidationResult::{Incomplete, Invalid, Valid};
 
@@ -98,11 +147,39 @@ impl Validator for TaskMasterHelper {
 	}
 }
 
-impl Helper for TaskMasterHelper {}
+impl Helper for TaskmasterHelper {}
+
+fn get_taskmaster_hints() -> Vec<CommandHint> {
+	let mut set = Vec::new();
+
+	set.push(CommandHint::new("help"));
+
+	set.push(CommandHint::new("global [status|start|stop|restart]"));
+
+	set.push(CommandHint::new("status <name>"));
+	set.push(CommandHint::new("start <name>"));
+	set.push(CommandHint::new("stop <name>"));
+	set.push(CommandHint::new("restart <name>"));
+
+	set.push(CommandHint::new("list [all|running|stopped|configs]"));
+
+	set.push(CommandHint::new("load <config>"));
+	set.push(CommandHint::new("unload <config>"));
+	set.push(CommandHint::new("reload <config>"));
+
+	set.push(CommandHint::new("logs <name>"));
+
+	set
+}
 
 fn main() {
-	let helper = TaskMasterHelper { };
-	let mut rl = rustyline::Editor::<TaskMasterHelper>::new().unwrap();
+	let helper = TaskmasterHelper {
+		hints:       get_taskmaster_hints(),
+		highlighter: TaskmasterHighlighter::new(),
+	};
+	let mut rl = rustyline::Editor::<TaskmasterHelper>::new().unwrap();
+
+	rl.set_completion_type(rustyline::CompletionType::List);
 	rl.set_helper(Some(helper));
 
 	loop {
