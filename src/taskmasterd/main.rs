915d22d262ef3c1853c508955ca70cc090834efc
u <hi@mathias.ninja>
2023-02-01 12:29:10 +0100

    improved status

--- src/taskmasterd/main.rs
+++ src/taskmasterd/main.rs
@@ -1,7 +1,7 @@
 extern crate taskmastersocket;
 use taskmastersocket::{TaskmasterDaemonRequest, TaskmasterDaemonResult};
 
-use std::{collections::HashMap, process::Child, fs::File, os::unix::net::{UnixListener, UnixStream}, thread, io::Write, sync::{Mutex, Arc, MutexGuard}, time::Duration};
+use std::{collections::HashMap, process::Child, fs::File, os::unix::{net::{UnixListener, UnixStream}, process}, thread, io::Write, sync::{Mutex, Arc, MutexGuard}, time::{Duration, Instant}, fmt::format};
 
 use daemonize::Daemonize;
 use yaml_rust::Yaml;
@@ -30,26 +30,96 @@ struct TaskOptions {
 	numprocs: i64,
 }
 
-struct Task {
-	options: TaskOptions,
-	processes: Vec<Child>,
+enum ExitStatus {
+	NotRunning,
+	Running{since: Instant, pid: u32},
+	LaunchFailed{at: Instant, err: String},
+	Exited{at: Instant, code: i32},
+	Stopped{at: Instant},
+	Killed{at: Instant},
 }
 
-impl Task {
-	fn spawn_single(&mut self) {
-		match std::process::Command::new(&self.options.argv[0])
-			.args(&self.options.argv[1..])
+struct Process {
+	process: Option<Child>,
+	created_at: Instant,
+	retries_count: u64,
+	current_status: ExitStatus
+}
+
+impl Process {
+	fn new() -> Process {
+		Process {
+			process: None,
+			created_at: Instant::now(),
+			retries_count: 0,
+			current_status: ExitStatus::NotRunning
+		}
+	}
+
+	fn spawn(&mut self, opts: &TaskOptions) {
+		match std::process::Command::new(&opts.argv[0])
+			.args(&opts.argv[1..])
 			.spawn() {
 			Ok(child) => {
-				println!("\"{name}\" started", name = self.options.argv[0]);
-				self.processes.push(child);
+				println!("\"{name}\" started", name = opts.argv[0]);
+				self.current_status = ExitStatus::Running{since: Instant::now(), pid: child.id()};
+				self.process = Some(child);
 			},
 			Err(e) => {
-				eprintln!("\"{name}\" failed to start: {e}", name = self.options.argv[0]);
+				eprintln!("\"{name}\" failed to start: {e}", name = opts.argv[0]);
+				self.current_status = ExitStatus::LaunchFailed{at: Instant::now(), err: e.to_string()};
 			}
 		}
 	}
 
+	fn start(&mut self, opts: &TaskOptions) {
+		if self.process.is_some() {
+			return;
+		}
+
+		self.spawn(opts);
+
+		self.retries_count = 0;
+	}
+
+	fn stop(&mut self) {
+		if let Some(child) = &mut self.process {
+			child.kill();
+			self.process = None;
+			self.current_status = ExitStatus::Killed{at: Instant::now()};
+		}
+	}
+
+	fn health_check(&mut self, opts: &TaskOptions) {
+		if let Some(child) = &mut self.process {
+			if let Ok(Some(status)) = child.try_wait() {
+				self.retries_count += 1;
+				if let Some(code) = status.code() {
+					self.current_status = ExitStatus::Exited{at: Instant::now(), code};
+				}
+				self.spawn(opts);
+			}
+		}
+	}
+
+	fn status(&self) -> String {
+		(match &self.current_status {
+			ExitStatus::NotRunning => format!("\x1b[90mNot running"),
+			ExitStatus::Running{since, pid} => format!("\x1b[92mRunning (started {}s ago with pid {pid})", since.elapsed().as_secs()),
+			ExitStatus::LaunchFailed{at, err} => format!("\x1b[91mLaunch failed ({}s ago): {err}", at.elapsed().as_secs()),
+			ExitStatus::Exited{at, code} => format!("\x1b[91mExited ({}s ago) with code {code}", at.elapsed().as_secs()),
+			ExitStatus::Stopped{at} => format!("\x1b[93mStopped ({}s ago)", at.elapsed().as_secs()),
+			ExitStatus::Killed{at} => format!("\x1b[93mKilled ({}s ago)", at.elapsed().as_secs()),
+		}) + &format!("\x1b[90m (created {}s ago, {} retries)\x1b[0m", self.created_at.elapsed().as_secs(), self.retries_count)
+	}
+}
+
+struct Task {
+	options: TaskOptions,
+	processes: Vec<Process>,
+}
+
+impl Task {
 	fn clone_options(&self) -> Task {
 		Task {
 			options: self.options.clone(),
@@ -58,17 +128,19 @@ impl Task {
 	}
 
 	fn start(&mut self) {
-		println!("start task...");
-		for i in self.processes.len()..self.options.numprocs as usize {
-			self.spawn_single()
+		while self.processes.len() < self.options.numprocs as usize {
+			self.processes.push(Process::new());
+		}
+
+		for process in &mut self.processes {
+			process.start(&self.options);
 		}
 	}
 
 	fn stop(&mut self) {
-		for child in &mut self.processes {
-			child.kill().expect("Could not kill child process.");
+		for process in &mut self.processes {
+			process.stop();
 		}
-		self.processes = Vec::new();
 	}
 
 	fn update(&mut self, options: TaskOptions) {
@@ -82,20 +154,8 @@ impl Task {
 	}
 
 	fn health_check(&mut self) {
-		let mut i = 0;
-		while i < self.processes.len() {
-			let child = &mut self.processes[i];
-			if let Ok(Some(status)) = child.try_wait() {
-				let code = status.code();
-				if status.success() {
-					println!("\"{name}\"[{i}] exited with status code {code:?}", name = self.options.argv[0]);
-				} else {
-					println!("\"{name}\"[{i}] exited with status code {code:?}", name = self.options.argv[0]);
-				}
-				self.processes.remove(i);
-				self.spawn_single();
-			}
-			i += 1;
+		for process in &mut self.processes {
+			process.health_check(&self.options);
 		}
 	}
 }
@@ -239,7 +299,11 @@ impl TaskFiles {
 }
 
 fn bind(path: &str) -> std::io::Result<UnixListener> {
-	std::fs::remove_file(path)?;
+	if let Err(err) = std::fs::remove_file(path) {
+		if err.kind() != std::io::ErrorKind::NotFound {
+			return Err(err);
+		}
+	}
 	UnixListener::bind(path)
 }
 
@@ -247,10 +311,21 @@ fn bind(path: &str) -> std::io::Result<UnixListener> {
 fn handle_client_request(tasks: &mut MutexGuard<TaskFiles>, req: TaskmasterDaemonRequest) -> TaskmasterDaemonResult {
 	match req {
 		TaskmasterDaemonRequest::Status => {
+			if tasks.tasks_files.is_empty() {
+				return TaskmasterDaemonResult::Ok("No tasks loaded yet".to_owned());
+			}
+
 			let mut status = String::new();
 			for task_file in tasks.tasks_files.values() {
+				if !status.is_empty() {
+					status.push_str("\n");
+				}
+				status.push_str(&format!("{}:\n", task_file.path));
 				for (name, task) in task_file.tasks.iter() {
-					status.push_str(&format!("{}: {}: {:?}\n", task_file.path, name, task.processes.iter().map(|p| p.id()).collect::<Vec<u32>>()));
+					status.push_str(&format!("\n  {}:\n", name));
+					for i in 0..task.processes.len() {
+						status.push_str(&format!("    [{i}] -> {}\n", task.processes[i].status()));
+					}
 				}
 			}
 			TaskmasterDaemonResult::Raw(status)
